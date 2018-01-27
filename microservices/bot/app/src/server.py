bf460d66d6d800652e04b364d6dd856ca8e9f3bf
Mayank Padhi <mayankpadhi@live.com>
2018-01-28 00:35:25 +0530

    second Commit

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -1,3 +1,7 @@
+#Key: 5111f0da1fe0402ba1f08867dc513e8b
+# sports
+# /alexa-yoda-skill/microservices/bot/app/src
+
 from src import app
 from flask_ask import Ask, statement, question, session
 import json
@@ -5,29 +9,61 @@ import requests
 import time
 import unidecode
 import random
+import datetime
+from collections import namedtuple
 
+now = datetime.datetime.now()
 
 ask = Ask(app, "/yoda_quotes")
-
-def getYodaQuote():
-	url = "http://data.hasura/v1/query"
+def getEntertainment():
+	url = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&pagesize=1&page=2&apiKey=5111f0da1fe0402ba1f08867dc513e8b"
 	headers = {
 		"Content-Type": "application/json",
-        "X-Hasura-User-Id": "1",
-        "X-Hasura-Role": "admin"
+		"X-api-key": "5111f0da1fe0402ba1f08867dc513e8b"
 	}
-	body = {
-	    "type": "run_sql",
-	    "args": {
-	        "sql": "SELECT quote FROM yoda_quotes ORDER BY RANDOM() limit 1;"
-	    }
-	}
-	
-	response = requests.request("POST", url, data=json.dumps(body), headers=headers)
+	response= requests.get(url)
 	respObj = response.json()
-	print (respObj)
-	return (respObj["result"][1][0])
+	for element in respObj['articles']:
+		x= element['description']
+	return x;
 
+def getSports():
+	url = "https://newsapi.org/v2/top-headlines?country=in&category=sports&pagesize=1&page=2&apiKey=5111f0da1fe0402ba1f08867dc513e8b"
+	headers = {
+		"Content-Type": "application/json",
+		"X-api-key": "5111f0da1fe0402ba1f08867dc513e8b"
+	}
+	response= requests.get(url)
+	respObj = response.json()
+	for element in respObj['articles']:
+		x= element['description']
+	return x;
+
+def getHistory():
+	url = "http://numbersapi.com/"+str(now.month)+"/"+str(now.day)+"/date?json"
+	response= requests.get(url)
+	respObj = response.json()
+	return respObj['text']
+
+def getTrivia():
+	url = "http://numbersapi.com/random/trivia?json"
+	response= requests.get(url)
+	respObj = response.json()
+	return respObj['text']
+
+def getYodaQuote():
+	rand= random.randint(1, 4);
+	if rand== 1 :
+		return getEntertainment()
+
+	elif rand== 2 :
+		return getSports()
+
+	elif rand== 3 :
+		return getHistory()
+
+	else:
+		return getTrivia()
 
 @app.route('/')
 def homepage():
