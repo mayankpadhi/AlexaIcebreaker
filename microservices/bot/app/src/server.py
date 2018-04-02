42f8c6567ad25209039f7244357d9188ce678cdc
Mayank Padhi <mayankpadhi@live.com>
2018-04-02 20:16:09 +0530

    Update server.py

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -11,9 +11,22 @@ from collections import namedtuple
 now = datetime.datetime.now()
 
 ask = Ask(app, "/yoda_quotes")
-def getNews(category):
+def getEntertainment():
 	rand= random.randint(1, 9);
-	url = "https://newsapi.org/v2/top-headlines?country=in&category="+category+"&pagesize=1&page="+str(rand)+"&apiKey=5111f0da1fe0402ba1f08867dc513e8b"
+	url = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&pagesize=1&page="+str(rand)+"&apiKey=5111f0da1fe0402ba1f08867dc513e8b"
+	headers = {
+		"Content-Type": "application/json",
+		"X-api-key": "5111f0da1fe0402ba1f08867dc513e8b"
+	}
+	response= requests.get(url)
+	respObj = response.json()
+	for element in respObj['articles']:
+		x= "did you know "+element['description']
+	return x;
+
+def getSports():
+	rand= random.randint(1, 9);
+	url = "https://newsapi.org/v2/top-headlines?country=in&category=sports&pagesize=1&page="+str(rand)+"&apiKey=5111f0da1fe0402ba1f08867dc513e8b"
 	headers = {
 		"Content-Type": "application/json",
 		"X-api-key": "5111f0da1fe0402ba1f08867dc513e8b"
@@ -39,10 +52,10 @@ def getTrivia():
 def getYodaQuote():
 	rand= random.randint(1, 4);
 	if rand== 1 :
-		return getNews("Entertainment")
+		return getEntertainment()
 
 	elif rand== 2 :
-		return getNews("Sports")
+		return getSports()
 
 	elif rand== 3 :
 		return getHistory()
@@ -57,16 +70,22 @@ def homepage():
 @ask.launch
 def startSkill():
     quote = getYodaQuote()
-    response = quote + '...... mmmmmmm ......... Do you want more?'
+    response = quote + '.. hmmm Do you want more?'
     return question(response)
 
 @ask.intent("YesIntent")
 def shareQuote():
     quote = getYodaQuote()
-    response = quote + '...... mmmmmmm ......... Do you want more?'
+    response = quote + '.. hmmm  Do you want more?'
     return question(response)
 
 @ask.intent("NoIntent")
 def noIntent():
     byeText = 'Fine... OK... Bye'
     return statement(byeText)
+
+
+@ask.intent("Help")
+def noIntent():
+    helpText = 'Just say... Alexa, launch nice icebreaker'
+    return question(helpText)
